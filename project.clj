99e86847dd95a48ee109edc117571ee1819607e0
Joshua Pore <josh@auspore.com>
2014-09-25 19:44:17 +1000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojometer "0.1.0-SNAPSHOT"
+  :description "A simple clojure app to parse the pedometer data from iPod nano 6th gen"
+  :url "http://example.com/FIXME"
+  :license {:name "Apache Licence 2.0"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot clojometer.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
