577d6d8592814ede104a09609d4b743c486a19a1
Joshua Pore <josh64@gmail.com>
2014-09-27 10:21:30 +1000

    added data.xml dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "A simple clojure app to parse the pedometer data from iPod nano 6th gen"
   :url "http://example.com/FIXME"
   :license {:name "Apache Licence 2.0"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+		[org.clojure/data.xml "0.0.8"]]
   :main ^:skip-aot clojometer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
