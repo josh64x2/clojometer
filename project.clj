af76d659a8e96672d9dfe00e2a0997e41499be64
Joshua Pore <josh@auspore.com>
2014-09-27 12:04:29 +1000

    initial parsing test

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojometer "0.1.0-SNAPSHOT"
   :description "A simple clojure app to parse the pedometer data from iPod nano 6th gen"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/josh64x2/clojometer"
   :license {:name "Apache Licence 2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 		[org.clojure/data.xml "0.0.8"]]
--- src/clojometer/core.clj
+++ src/clojometer/core.clj
@@ -1,7 +1,13 @@
 (ns clojometer.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.xml :as xml]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (def ped-xml 
+	  (->(clojure.java.io/reader "ped-example.xml")
+  		(xml/parse)))
+		(println (for [x (xml-seq ped-xml)
+			:when (= :distanceString (:tag x))]
+		    (first (:content x)))))
